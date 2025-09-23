7f5e2323818d7db18c9efb5400b48d05bfa5db9d
Arne Decant <hello@arnedecant.be>
2025-09-23 21:05:31 +0200

    refactoring

--- src/modules/webgl/data/config.ts
+++ src/modules/webgl/data/config.ts
@@ -2,9 +2,9 @@ import imgHeart from '@/assets/heart.jpg'
 import imgDrop from '@/assets/codrops.jpg'
 import imgSmile from '@/assets/smile.jpg'
 import imgKiswe from '@/assets/kiswe.jpg'
-import vidPinkLiquid from '@/assets/pink-liquid.mp4'
-import vidBlueLiquid from '@/assets/blue-liquid.mp4'
-import vidPurpleLiquid from '@/assets/purple-liquid.mp4'
+import vidConcert from '@/assets/concert.mp4'
+import vidCrowd from '@/assets/crowd.mp4'
+import vidFuture from '@/assets/future.mp4'
 import vidKisweSizzle from '@/assets/kiswe-sizzle.mp4'
 import { ProjectionConfig } from '@/modules/webgl/types'
 
@@ -13,30 +13,30 @@ const CONFIG: ProjectionConfig = {
   spacing: 0.75,
   cubeW: 0.5,
   cubeH: 0.5,
-  elevation: 1.5,
+  elevation: 2,
   elevationStep: 0.8,
   elevationMargin: 0.5, // margin between pointer <=> grid
   items: [
     {
       id: 'heart',
       mask: imgHeart,
-      video: vidPinkLiquid
+      video: vidConcert
+    },
+    {
+      id: 'drop',
+      mask: imgDrop,
+      video: vidCrowd
+    },
+    {
+      id: 'smile',
+      mask: imgSmile,
+      video: vidFuture
     },
     // {
-    //   id: 'drop',
-    //   mask: imgDrop,
-    //   video: vidBlueLiquid
-    // },
-    // {
-    //   id: 'smile',
-    //   mask: imgSmile,
-    //   video: vidPurpleLiquid
-    // },
-    {
-      id: 'kiswe',
-      mask: imgKiswe,
-      video: vidKisweSizzle
-    }
+    //   id: 'kiswe',
+    //   mask: imgKiswe,
+    //   video: vidKisweSizzle
+    // }
   ]
 }
 
